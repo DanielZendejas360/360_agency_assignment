88267f2c0e9733998094b918a48c33eca76f7ec1
DanielZendejas <daniel8647@hotmail.com>
2022-03-14 19:25:32 -0600

    Introduc ListingController with empty actions

---
+++ src/main/java/com/daniel/Listings/controllers/ListingController.java
@@ -0,0 +1,36 @@
+package com.daniel.Listings.controllers;
+
+import com.daniel.Listings.entity.Listing;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ListingController {
+
+    @PostMapping("/listings")
+    public void create(@RequestBody Listing listing) {
+        throw new UnsupportedOperationException();
+    }
+
+    @PutMapping("/listings")
+    public void update(@RequestBody Listing listing) {
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/dealers/{dealerId}/listings")
+    public void get(@PathVariable UUID dealerId, @RequestParam Listing.State state) {
+        throw new UnsupportedOperationException();
+    }
+
+    @PostMapping("/listings/{listingId}/publish")
+    public void publish(@PathVariable UUID listingId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @PostMapping("/listings/{listingId}/unpublish")
+    public void unpublish(@PathVariable UUID listingId) {
+        throw new UnsupportedOperationException();
+    }
+}
