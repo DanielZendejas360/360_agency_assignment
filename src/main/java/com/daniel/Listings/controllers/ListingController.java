33d87e691e04bc4f1ef7fec2284f6b532cf745da
DanielZendejas <daniel8647@hotmail.com>
2022-03-16 20:01:03 -0600

    Give precedence to dealer id in path when creating a listing

--- src/main/java/com/daniel/Listings/controllers/ListingController.java
+++ src/main/java/com/daniel/Listings/controllers/ListingController.java
@@ -18,22 +18,13 @@ public class ListingController {
 
     @PostMapping("/listings")
     public Listing create(@PathVariable UUID dealerId, @RequestBody Listing listing) {
-        if (listing.getDealerId() != null && listing.getDealerId() != dealerId) {
-            String errorMessage = String.format(
-                    "Found mismatching dealer ids in path (%s) and request body (%s)",
-                    dealerId,
-                    listing.getDealerId());
-            throw new IllegalArgumentException(errorMessage);
-        }
-
-        if (listing.getDealerId() == null)
-            listing.setDealerId(dealerId);
-
+        listing.setDealerId(dealerId);
         return listingService.save(listing);
     }
 
     @PutMapping("/listings")
     public Listing update(@PathVariable UUID dealerId, @RequestBody Listing listing) {
+
         throw new UnsupportedOperationException();
     }
 
