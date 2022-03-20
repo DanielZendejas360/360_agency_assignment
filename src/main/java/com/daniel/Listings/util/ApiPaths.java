1174e35b13b65c21aa5da632e134b6500a258fa2
DanielZendejas360 <danielzendejas360@gmail.com>
2022-03-19 21:16:48 -0600

    Use context-path /api/v1

--- src/main/java/com/daniel/Listings/util/ApiPaths.java
+++ src/main/java/com/daniel/Listings/util/ApiPaths.java
@@ -1,9 +1,9 @@
 package com.daniel.Listings.util;
 
 public class ApiPaths {
-    public static final String LISTINGS_CREATE = "/api/v1/dealers/{dealerId}/listings";
-    public static final String LISTINGS_UPDATE = "/api/v1/dealers/{dealerId}/listings/{listingId}";
-    public static final String LISTINGS_GET_BY_DEALER_AND_STATE = "/api/v1/dealers/{dealerId}/listings";
-    public static final String LISTINGS_PUBLISH = "/api/v1/dealers/{dealerId}/listings/{listingId}/publish";
-    public static final String LISTINGS_UNPUBLISH = "/api/v1/dealers/{dealerId}/listings/{listingId}/unpublish";
+    public static final String LISTINGS_CREATE = "/dealers/{dealerId}/listings";
+    public static final String LISTINGS_UPDATE = "/dealers/{dealerId}/listings/{listingId}";
+    public static final String LISTINGS_GET_BY_DEALER_AND_STATE = "/dealers/{dealerId}/listings";
+    public static final String LISTINGS_PUBLISH = "/dealers/{dealerId}/listings/{listingId}/publish";
+    public static final String LISTINGS_UNPUBLISH = "/dealers/{dealerId}/listings/{listingId}/unpublish";
 }
